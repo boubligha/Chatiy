562e3f6b1b978b4032bf009eb69338b45e0167c4
boubligha <151193079+boubligha@users.noreply.github.com>
2025-01-31 12:09:06 +0100

    login Page Ui

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/foundation.dart';
 import 'package:mychatflutter_app/pages/login_page.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await Firebase.initializeApp(
@@ -22,28 +23,22 @@ void main() async {
     await Firebase.initializeApp();
   }
 
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Chatiy',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Color.fromRGBO(42, 117, 188, 1),
-        colorScheme: ColorScheme.dark(
-          primary: Color.fromRGBO(42, 117, 188, 1),
-          secondary: Color.fromRGBO(42, 117, 188, 1),
-          background: Color.fromRGBO(28, 27, 27, 1),
-        ),
-      ),
-      home: LogingPage(),
-    );
+  debugShowCheckedModeBanner: false,
+  title: 'Chatiy',
+  theme: ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Color.fromRGBO(42, 117, 188, 1),
+    hintColor: Color.fromRGBO(42, 117, 188, 1),
+    scaffoldBackgroundColor: Color.fromRGBO(28, 27, 27, 1), // Corrected parameter
+  ),
+  home: LoginPage(), // Ensure this matches the class name
+);
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,17 +1,155 @@
 import 'package:flutter/material.dart';
 
-class LogingPage extends StatefulWidget {
-   LogingPage({super.key});
+class LoginPage extends StatefulWidget {
+  LoginPage({super.key});
 
   @override
-  State<LogingPage> createState() => _LogingPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LogingPageState extends State<LogingPage> {
+class _LoginPageState extends State<LoginPage> {
+  late double _deviceHeight, _deviceWidth;
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Align(
+        alignment: Alignment.center,
+        child: _loginPageUI(),
+      ),
+    );
+  }
+
+  Widget _loginPageUI() {
+    return Container(
+      height: _deviceHeight * 0.60,
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.10),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _headingWidget(),
+          _inputForm(),
+        ],
+      ),
+    );
+  }
+
+  Widget _headingWidget() {
+    return Container(
+      height: _deviceHeight * 0.12,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Welcome Back!",
+            style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            "Please login to your account.",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w200,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _inputForm() {
+    return Container(
+      height: _deviceHeight * 0.3,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _emailTextField(),
+            _passwordTextField(),
+            _loginButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _emailTextField() {
+    return TextFormField(
+      autocorrect: false,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {
+        if (_input!.isEmpty) {
+          return "Please enter your email";
+        }
+        if (!_input.contains("@")) {
+          return "Please enter a valid email";
+        }
+        return null;
+      },
+      onSaved: (newValue) {
+        // Save email value
+      },
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        hintText: "Email",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+      obscureText: true, // Hide password
+      autocorrect: false,
+      style: TextStyle(color: Colors.white),
+      validator: (_input) {
+        if (_input!.isEmpty) {
+          return "Please enter your password";
+        }
+        return null;
+      },
+      onSaved: (newValue) {
+        // Save password value
+      },
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        hintText: "Password",
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _loginButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_formKey.currentState!.validate()) {
+          _formKey.currentState!.save();
+          // Perform login logic
+        }
+      },
+      child: Text("Login"),
     );
   }
 }
